a4de8220558becbf16f3562eaceed68932ac0c4d
Xisca04 <xiscacerdavicens@gmail.com>
2022-11-15 13:07:03 +0100

    Final_proyecto1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20f;
-    public float turnSpeed = 10f;
+    private float speed = 20f;
+    private float turnSpeed = 10f;
     private float horizontalInput;
+    private float verticalInput;
     void Update()
     {
-        //Movimiento hacia delante
-        //transform.Translate(0, 0, 1);
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
-       
-        //Detectar el Input Horizontal
+        //Detectar el Input Horizontal y Vertical
         horizontalInput = Input.GetAxis("Horizontal");
-        //Movimiento lateral manual
+        verticalInput = Input.GetAxis("Vertical");
+
+        //Movimiento hacia delante manual
+        //transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
+        /*
+        Movimiento lateral manual
         transform.Translate(Vector3.right * turnSpeed * Time.deltaTime * horizontalInput);
+       */
+        //Movimineto rotatorio manual
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
     }
 }
